e37dfe36e3fda87efd2d2d5646129fdd17426c21
Norbert Nopper <norbert@nopper.tv>
2013-10-01 21:28:47 +0200

    Revert "F"
    This reverts commit 69c57fde031903444eefbcb9f0a65d13a8256bf2.

--- Example30/shader/raytrace.comp.glsl
+++ Example30/shader/raytrace.comp.glsl
@@ -15,7 +15,7 @@ layout (binding = 0, rgba8) uniform image2D u_texture;
 layout (std430, binding = 1) buffer Directions
 {
 	vec3 direction[];
-	// Padding[]
+	// Padding
 } b_directions;
 
 layout (std430, binding = 2) buffer Positions
@@ -49,7 +49,7 @@ struct Material {
 
 	float reflectivity;
 
-	// Padding
+	float padding;
 };
 
 struct Sphere
@@ -58,7 +58,7 @@ struct Sphere
 
 	float radius;
 
-	// Padding[3]
+	vec3 padding;
 
 	Material material;
 };
